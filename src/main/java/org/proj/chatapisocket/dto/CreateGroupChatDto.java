83055fb04bb201e8c55014b29f295aa25cc6c5b2
TyZeer <d.pavlov04@gmail.com>
2025-04-10 23:44:33 +0300

    i forgor

--- src/main/java/org/proj/chatapisocket/dto/CreateGroupChatDto.java
+++ src/main/java/org/proj/chatapisocket/dto/CreateGroupChatDto.java
@@ -1,5 +1,6 @@
 package org.proj.chatapisocket.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +15,6 @@ import java.util.Set;
 public class CreateGroupChatDto {
 
     private String name;
-
+    @JsonProperty("member_ids")
     private Set<Long> memberIds;
 }
