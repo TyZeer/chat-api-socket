e9fb6f0519901f3e07f671b64afad2859402a77c
TyZeer <d.pavlov04@gmail.com>
2025-04-13 17:35:35 +0300

    Added user search and upd doc

--- src/main/java/org/proj/chatapisocket/controllers/UserController.java
+++ src/main/java/org/proj/chatapisocket/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.proj.chatapisocket.repos.UserRepository;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,12 +22,18 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping()
-    public List<UserDto> getUsers(@AuthenticationPrincipal User currentUser) {
-        Long userId = currentUser.getId();
+    @GetMapping
+    public List<UserDto> getUsers(
+            @AuthenticationPrincipal User currentUser,
+            @RequestParam(required = false) String username
+    ) {
+        Long currentUserId = currentUser.getId();
+
         return userRepository.findAll().stream()
-                .filter(u -> !u.getId().equals(userId))
-                .map(u -> new UserDto(u.getId(), u.getUsername()))
+                .filter(user -> !user.getId().equals(currentUserId))
+                .filter(user -> username == null || user.getUsername().toLowerCase().contains(username.toLowerCase()))
+                .map(user -> new UserDto(user.getId(), user.getUsername()))
                 .collect(Collectors.toList());
     }
+
 }
