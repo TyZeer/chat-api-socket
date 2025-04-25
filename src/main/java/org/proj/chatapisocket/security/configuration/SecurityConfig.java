12179893b315b722a9fd9d5d1cac0c39f1f51fc9
TyZeer <d.pavlov04@gmail.com>
2025-04-26 01:50:47 +0300

    Отключил корс

--- src/main/java/org/proj/chatapisocket/security/configuration/SecurityConfig.java
+++ src/main/java/org/proj/chatapisocket/security/configuration/SecurityConfig.java
@@ -35,14 +35,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
         .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(request -> {
-                    var corsConfiguration = new CorsConfiguration();
-                    corsConfiguration.setAllowedOriginPatterns(List.of("*"));
-                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                    corsConfiguration.setAllowedHeaders(List.of("*"));
-                    corsConfiguration.setAllowCredentials(true);
-                    return corsConfiguration;
-                }))
+                .cors(AbstractHttpConfigurer::disable)
+//                .cors(cors -> cors.configurationSource(request -> {
+//                    var corsConfiguration = new CorsConfiguration();
+//                    corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+//                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//                    corsConfiguration.setAllowedHeaders(List.of("*"));
+//                    corsConfiguration.setAllowCredentials(true);
+//                    return corsConfiguration;
+//                }))
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/auth/**", "/ws/**").permitAll()
                         .requestMatchers("/ws/**", "/app/**", "/topic/**").permitAll()
